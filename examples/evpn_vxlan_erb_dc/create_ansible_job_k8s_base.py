b1aa2aaab7ef1c02d5930d0a9c1f88fb65a702a0
Adrian-STA <127490404+Adrian-STA@users.noreply.github.com>
2023-09-19 16:51:43 +0200

    Create create_ansible_job_k8s_base.py

---
+++ examples/evpn_vxlan_erb_dc/create_ansible_job_k8s_base.py
@@ -0,0 +1,8 @@
+import yaml
+import os
+path = os.getcwd()
+with open('nita-ansible-job-base.yaml', 'r') as file:
+    data = yaml.load(file, Loader=yaml.FullLoader)
+    data['spec']['template']['spec']['containers'][0]['workingDir'] = path
+    with open('nita-ansible-job-base.yaml', 'w') as file:
+        yaml.dump(data, file)
