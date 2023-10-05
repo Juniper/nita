f63cbf2814083dc166c7cea975e8506dca7f5895
Adrian-STA <127490404+Adrian-STA@users.noreply.github.com>
2023-10-05 16:31:19 +0200

    Delete examples/ebgp_wan/create_ansible_job_k8s.py

--- examples/ebgp_wan/create_ansible_job_k8s.py
+++
@@ -1,8 +0,0 @@
-import yaml
-import os
-path = os.getcwd()
-with open('nita-ansible-job1.yaml', 'r') as file:
-    data = yaml.load(file, Loader=yaml.FullLoader)
-    data['spec']['template']['spec']['containers'][0]['workingDir'] = path
-    with open('nita-ansible-job1.yaml', 'w') as file:
-        yaml.dump(data, file)
